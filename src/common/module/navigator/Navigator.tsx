96638aec1fcc6542a40676f3372d77dd71ddff92
MinseoLee <mmmm@ajou.ac.kr>
2023-10-28 23:03:34 +0900

    apply css on gloabls

--- src/common/module/navigator/Navigator.tsx
+++ src/common/module/navigator/Navigator.tsx
@@ -1,8 +1,25 @@
 import S from './Navigator.module.css';
+import {Link} from "react-router-dom";
 
 const Navigator = () => {
 	return (
-		<nav className={S['navigator-container']}>nav</nav>
+		<nav className={S['navigator-container']}>
+			<Link to={'/'}>
+				<div className={S['navigator-item']}>🏠 Home</div>
+			</Link>
+			<Link to={'/rank'}>
+				<div className={S['navigator-item']}>👑 Ranks</div>
+			</Link>
+			<Link to={'/honor'}>
+				<div className={S['navigator-item']}>🎊 Honors</div>
+			</Link>
+			<Link to={'/credits'}>
+				<div className={S['navigator-item']}>🙇‍ Credits</div>
+			</Link>
+			<Link to={'/login'}>
+				<div className={S['navigator-item']}>🔓‍ Logout</div>
+			</Link>
+		</nav>
 	);
 }
 

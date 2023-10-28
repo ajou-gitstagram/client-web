035b6f253c5da7ac9ef863be6b8e31b10c2022cd
MinseoLee <mmmm@ajou.ac.kr>
2023-10-28 18:57:19 +0900

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
+import './common/reset.css';
 import {Routes, Route} from "react-router-dom";
 import HomePage from "./pages/home-page/HomePage";
 import RankPage from "./pages/rank-page/RankPage";
 import LoginPage from "./pages/login-page/LoginPage";
 import SignupPage from "./pages/signup-page/SignupPage";
 import PostPage from "./pages/post-page/PostPage";
+import Header from "./common/module/Header";
 
 function App() {
     return (
         <div className="App">
+            <Header />
             <Routes>
                 <Route path={"/"} element={<HomePage />} />
                 <Route path={"/login"} element={<LoginPage />} />
---
+++ src/common/module/Header.tsx
@@ -0,0 +1,11 @@
+import S from './Header.module.css';
+
+const Header = () => {
+	return (
+		<header className={S['header-container']}>
+			<p>Gitstagram</p>
+		</header>
+	);
+}
+
+export default Header;

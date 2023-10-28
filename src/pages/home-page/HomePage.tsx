3b0c732653db3641a7d80bfb02257c7dc92abe0b
MinseoLee <mmmm@ajou.ac.kr>
2023-10-28 19:40:26 +0900

    set fixed position

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,8 +1,22 @@
-import "./HomePage.module.css";
+import S from "./HomePage.module.css";
+import ContentsBox from "./contents-box/ContentsBox";
+import Blank from "../../common/module/blank/Blank";
+
+const dummy = [
+    {},
+    {},
+    {},
+    {},
+]
 
 const HomePage = () => {
     return (
-        <div>home</div>
+        <div className={S['container']}>
+            <Blank height={48} />
+            { dummy.map((d, i) => {
+                return <ContentsBox />
+            }) }
+        </div>
     );
 }
 

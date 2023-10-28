5e2b94c5444b90ebd1c7840641f7d40aaf11af11
kmkkkp <kmkkkp@ajou.ac.kr>
2023-10-29 01:43:35 +0900

    Fix PostingPage

--- src/pages/commit-rank-page/config/model.ts
+++ src/pages/commit-rank-page/config/model.ts
@@ -3,7 +3,6 @@ type RankModel = Array<RankData>;
 interface RankData {
 	id: string;
 	commits: number;
-	rank: number;
 }
 
 export type {
--- src/pages/posting-rank-page/PostingRankPage.tsx
+++ src/pages/posting-rank-page/PostingRankPage.tsx
@@ -1,22 +1,21 @@
 import S from "./PostingRankPage.module.css";
 import {HomeModel} from "../home-page/config/model";
 import Blank from "../../common/module/blank/Blank";
-import ContentsBox from "../home-page/contents-box/ContentsBox";
-import React from "react";
+import React, {useState} from "react";
 
 const dummy: HomeModel = [
 	{
 		uid: 1,
 		id: "id000",
 		contents: "contentscontents",
-		likes: 100,
+		likes: 90,
 		tags: ["tag1", "tag2", "tag3"]
 	},
 	{
 		uid: 2,
 		id: "id111",
 		contents: "contentscontents",
-		likes: 99,
+		likes: 109,
 		tags: ["tag1", "tag2", "tag3"]
 	},
 	{
@@ -36,19 +35,56 @@ const dummy: HomeModel = [
 ]
 
 const PostingRankPage = () => {
+
+	const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth() + 1)
+	
+	const handleMonthChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		setSelectedMonth(parseInt(e.target.value, 10));
+	};
+
+	const sortedDummy = [...dummy].sort((a, b) => b.likes - a.likes);
+
 	return (
-		<div className={S['container']}>
-			<Blank height={48} />
-			<h2 style={{fontSize: "50px", marginBottom: "20px"}}>Month of </h2>
-			{ dummy.map((d, i) => {
-				return (
-					<>
-						<ContentsBox data={d} />
-					</>
-				);
-			}) }
+	<div>
+		<div>
+			<div style={{marginTop:"10%"}}>
+				<label htmlFor="month">Month:</label>
+				<select id="month" value={selectedMonth} onChange={handleMonthChange}>
+				{Array.from({ length: 12 }, (_, index) => index + 1).map((month) => (
+					<option key={month} value={month}>
+					{month}
+					</option>
+				))}
+				</select>
+			</div>
+			<h2 style={{fontSize: "50px", marginBottom: "20px"}}>🧡 Rank Month of {selectedMonth}</h2>
+		
+			<div className={S['ranking']}>
+				{ sortedDummy.map((d, i) => {
+					return <ContentsBox data={d} rank={i+1}/>
+				}) }
+			</div>
 		</div>
+	</div>
 	);
 };
+
+const getMedal = (idx: number) => {
+	if (idx === 1) return "🥇";
+	else if (idx === 2) return "🥈";
+	else if (idx === 3) return "🥉";
+	else return "";
+}
+
+const ContentsBox: React.FC<{ data: { id: string, likes: number }, rank: number}> = ({ data, rank }) => {
+   
+	return (
+        <div className={S['rankItem']}>
+			<span className={S["rank"]}>{rank}{getMedal(rank)}</span>
+            <span className={S['id']}>{data.id}</span>
+            <span className={S['likes']}>{data.likes} 🧡</span>
+        </div>
+    );
+}
 
 export default PostingRankPage;
